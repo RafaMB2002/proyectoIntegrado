c6e318fff353b19631b526669c5782ca5b843314
rafamb <rmonber2302@g.educaand.es>
2023-05-19 18:24:32 +0200

    Pantalla de pedidos y generar qr

--- src/Controller/PedidosController.php
+++ src/Controller/PedidosController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Repository\BebidaRepository;
+use App\Repository\PlatoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PedidosController extends AbstractController
 {
-    #[Route('/pedidos', name: 'app_pedidos')]
-    public function index(): Response
+    #[Route('/generarQR', name: 'generarQR')]
+    public function generarQR(): Response
     {
-        return $this->render('pedidos/index.html.twig', [
+        return $this->render('pedidos/qr.html.twig', [
             'controller_name' => 'PedidosController',
+        ]);
+    }
+
+    #[Route('/pedidos', name: 'pedidos')]
+    public function pedidos(PlatoRepository $platoRepository, BebidaRepository $bebidaRepository): Response
+    {
+        $platos = $platoRepository->findAll();
+        $bebidas = $bebidaRepository->findAll();
+
+        return $this->render('pedidos/pedidos.html.twig', [
+            'controller_name' => 'PedidosController',
+            'platos' => $platos,
+            'bebidas' => $bebidas
         ]);
     }
 }
